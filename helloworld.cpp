9bc484734b2430619a6c687a5cf958c2e6196480
Mike Murrell <mmurrell@indsci.com>
2023-05-31 12:07:55 -0400

    Pepper Jack is where it's at

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,6 +2,6 @@ include <iostream>
 
 int main(){
  std::cout << "Hello MAC N CHEESE. Cheddar is best. Goodbye!";
- 
+ std::cout << "whoever wrote this ^^^ is WROOOOOOOOOONG! its all bout dat pepperjack ";
  return 0;
 }
